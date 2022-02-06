19ebf6fe202fc7daf6e6c4a0fbc4d039df7c8891
eeesnghyun <szhyun2002@gmail.com>
2022-02-06 23:15:32 +0900

    thymeleaf layout dialect 적용

--- src/main/java/com/app/planm/approval/controller/ApprovalController.java
+++ src/main/java/com/app/planm/approval/controller/ApprovalController.java
@@ -1,15 +1,19 @@
 package com.app.planm.approval.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/approval")
 public class ApprovalController {
 
-	@GetMapping
-	public String home() {
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String home(Model model) {
+		
+		
+		
 		return "approval/index";
 	}
 	
--- src/main/java/com/app/planm/menu/controller/MenuController.java
+++ src/main/java/com/app/planm/menu/controller/MenuController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MenuController {
 
-	@RequestMapping(value="/browse", method=RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		
 		return "index";
 	}
+	
 }
